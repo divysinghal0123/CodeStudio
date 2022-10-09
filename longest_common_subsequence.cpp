97a060ee2c0b8ae23a33ae2fcc92f5b588089621
Divy Singhal <76985594+divysinghal0123@users.noreply.github.com>
2022-10-10 01:07:10 +0530

    Create longest_common_subsequence.cpp

---
+++ longest_common_subsequence.cpp
@@ -0,0 +1,26 @@
+// Solution
+
+//Tabulation Method
+
+
+#include <bits/stdc++.h> 
+int lcs(string &str1, string &str2){
+	//	Write your code here.
+    int n = str1.size();
+    int m = str2.size();
+    vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
+    int i,j;
+    for(i=0;i<=n;i++) dp[i][0]=0;
+    for(j=0;j<=m;j++) dp[0][j]=0;
+    int ans = 0;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=m;j++){
+            if(str1[i-1] == str2[j-1]){
+                dp[i][j] = 1 + dp[i-1][j-1];
+                ans = max(ans,dp[i][j]);
+            }else{
+                dp[i][j] = 0;
+            }
+        }
+    }
+    return ans;
